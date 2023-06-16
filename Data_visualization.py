84b468c1e1a7de080f70028a3ca047d25b415d02
Jaime Alvarez <134750217+alvarezjaime80@users.noreply.github.com>
2023-06-15 20:49:11 -0700

    New version using sys.argv
    Allows you to run program on multiple files if they are specified in

--- Data_visualization.py
+++ Data_visualization.py
@@ -4,101 +4,93 @@ import seaborn as sns
 from matplotlib import pyplot as plt
 import matplotlib.animation as animation
 from astropy import units as u
+import sys
 
-data = pd.read_csv(r"C:\Users\jaime\OneDrive\Documents\Jumpstart Data\3112020.csv")
 
-def get_pix(deg, pix_deg_conversion):
-    """function takes a value in degrees and returns converted value in pixels
-    parameter: deg - int, initial value in degrees
-    parameter: pix_deg_conversion - int, conversion ratio between pixels and degrees
-    returns: int, converted value in pixels
-    """
-    pix = deg.to(u.deg)*(pix_deg_conversion*(1/u.deg))
-    return pix.to('')
+for i in range(len(sys.argv) - 1):
+    file_name = sys.argv[1 + i]
 
-#finding mean value for right acension
-degree_ra_mean = data.kic_degree_ra.mean()
+    data = pd.read_csv(file_name)
 
-#finding mean value for declenation
-degree_dec_mean = data.kic_deg.mean()
+    def get_pix(deg, pix_deg_conversion):
+        """function takes a value in degrees and returns converted value in pixels
+        parameter: deg - int, initial value in degrees
+        parameter: pix_deg_conversion - int, conversion ratio between pixels and degrees
+        returns: int, converted value in pixels
+        """
+        pix = deg.to(u.deg)*(pix_deg_conversion*(1/u.deg))
+        return pix.to('')
 
-#setting x to the difference of the right ascension values from the mean
-x = [degree_ra_mean - i for i in data.kic_degree_ra]
+    #finding mean value for right acension
+    degree_ra_mean = data.kic_degree_ra.mean()
 
-#converting x values from degrees to pixels
-x_pix = [get_pix(i*u.deg, (60**2)/4)*10**6 for i in x]
+    #finding mean value for declenation
+    degree_dec_mean = data.kic_deg.mean()
 
-#setting y to the difference of the declenation values from the mean
-y = [degree_dec_mean - i for i in data.kic_deg]
+    #setting x to the difference of the right ascension values from the mean
+    x = [degree_ra_mean - i for i in data.kic_degree_ra]
 
-#converting y values from degrees to pixels
-y_pix = [get_pix(i*u.deg, (60**2)/4)*10**6 for i in y]
+    #converting x values from degrees to pixels
+    x_pix = [get_pix(i*u.deg, (60**2)/4)*10**6 for i in x]
 
-#converting time from second to years
-t_weeks = [((i*u.s).to(u.wk))/u.wk for i in data.t]
+    #setting y to the difference of the declenation values from the mean
+    y = [degree_dec_mean - i for i in data.kic_deg]
 
-#creating figure of size 12x12 inches
-plt.figure(figsize = (12,12))
-ax = plt.subplot()
+    #converting y values from degrees to pixels
+    y_pix = [get_pix(i*u.deg, (60**2)/4)*10**6 for i in y]
 
-#plotting x vs time in green
-plt.scatter(t_weeks, x_pix, color='#990000')
+    #converting time from second to weeks
+    t_weeks = [((i*u.s).to(u.wk))/u.wk for i in data.t]
 
-#plotting y vs time in navy
-plt.scatter(t_weeks, y_pix, color='#FFC72C')
+    #calling jumpstart style sheet
+    plt.style.use('jumpstart')
+    ax = plt.subplot()
 
-#labeling x-axis as Time in units of seconds
-plt.xlabel('Time [Weeks]', fontsize=18, labelpad=10)
+    #plotting x vs time in Cardinal Red
+    plt.scatter(t_weeks, x_pix, color='#990000')
 
-#labeling y-axis as difference from mean position in units of pixels
-plt.ylabel('Distance from Mean Position [$\mu$Pixels]', fontsize=18)
+    #plotting y vs time in Gold
+    plt.scatter(t_weeks, y_pix, color='#FFC72C')
 
-#setting ticks
-xticks = np.linspace(0, 13, 14, dtype='int')
-ax.set_xticks(xticks)
+    #labeling x-axis as Time in units of Weeks
+    plt.xlabel('Time [Weeks]')
 
-#rotating x-tick labels for visibility
-ax.xaxis.set_tick_params(rotation=0)
+    #labeling y-axis as difference from mean position in units of pixels
+    plt.ylabel('Distance from Mean Position [$\mu$Pixels]')
 
-#using exact x and y values as ticks
-ax.ticklabel_format(useMathText=True)
+    #setting ticks
+    xticks = np.linspace(0, 13, 14, dtype='int')
+    ax.set_xticks(xticks)
 
-#making xtick labels larger
-plt.xticks(fontsize=14)
-plt.yticks(fontsize=14)
+    #creating a legend for the plot
+    plt.legend(['x-position','y-position'])
 
-#creating a legend for the plot
-plt.legend(['x-position','y-position'], fontsize=22, shadow = True)
+    #adding a title to the plot
+    plt.title('Change in Position Over Time')
 
-#adding a title to the plot
-plt.title('Change in Position Over Time', fontsize=30, pad=10)
-plt.show()
+    plt.show()
 
-fig = plt.figure(figsize = (18,12))
-ax = plt.subplot(1,4,(1,3))
+    fig = plt.figure(figsize = (16, 12))
+    ax = plt.subplot(1,4,(1,3))
 
-scatter_plot = ax.scatter(x_pix, y_pix,c = t_weeks, cmap = 'rainbow', s=range(1010,10,-1))
+    scatter_plot = ax.scatter(x_pix, y_pix,c = t_weeks, cmap = 'rainbow', s=range(1010,10,-1))
 
-plt.xlabel('x-position [$\mu$Pixels]', fontsize=22, labelpad=15)
-plt.ylabel('y-position [$\mu$Pixels]', fontsize=22)
-plt.title('Y vs X Positions Over Time', fontsize=30)
-cbar = plt.colorbar(scatter_plot, label = "Time [s]")
-cbar.set_label("Time [Weeks]", size=20, labelpad=20)
-cbar.ax.tick_params(labelsize=18)
-ax.ticklabel_format(useMathText=True)
-#making tick labels larger
-plt.xticks(fontsize=18)
-plt.yticks(fontsize=18)
+    plt.xlabel('x-position [$\mu$Pixels]')
+    plt.ylabel('y-position [$\mu$Pixels]')
+    plt.title('Y vs X Positions Over Time')
+    cbar = plt.colorbar(scatter_plot, label = "Time [s]")
+    cbar.set_label("Time [Weeks]", size=34, labelpad=20)
+    cbar.ax.tick_params(labelsize=30)
 
-def update(frame):
-    x = x_pix[:frame]
-    y = y_pix[:frame]
-    c = t_weeks[:frame]
-    s = range(1010,10,-1)[:frame]
-    frame_data = np.stack([x, y, c]).T
-    scatter_plot.set_offsets(frame_data)
-    return scatter_plot
-    
-ani = animation.FuncAnimation(fig=fig, func=update, frames=1000, interval=10, blit=False)
+    def update(frame):
+        x = x_pix[:frame*20]
+        y = y_pix[:frame*20]
+        c = t_weeks[:frame*20]
+        s = range(1010,10,-1)[:frame*20]
+        frame_data = np.stack([x, y, c]).T
+        scatter_plot.set_offsets(frame_data)
+        return scatter_plot
+        
+    ani = animation.FuncAnimation(fig=fig, func=update, frames=50, interval=1, blit=False)
 
-plt.show()
+    plt.show()
